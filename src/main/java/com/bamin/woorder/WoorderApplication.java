1be8ca522319ed6b6a3137a613dbf02b049a07ae
woowahan-kwonmc <luffy@woowahan.com>
2020-01-07 14:24:47 +0900

    Spring Boot 프로젝트 생성

---
+++ src/main/java/com/bamin/woorder/WoorderApplication.java
@@ -0,0 +1,13 @@
+package com.bamin.woorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bamin/woorder/WoorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamin.woorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

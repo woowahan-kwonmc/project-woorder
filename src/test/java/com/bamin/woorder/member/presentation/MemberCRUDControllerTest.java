93b72a8d8214f42bce8b8827d0a27fce6ae2481f
woowahan-kwonmc <luffy@woowahan.com>
2020-01-10 16:10:05 +0900

    [#21] test - 문자열 상수로 교체

--- src/test/java/com/bamin/woorder/member/presentation/MemberCRUDControllerTest.java
+++ src/test/java/com/bamin/woorder/member/presentation/MemberCRUDControllerTest.java
@@ -1,6 +1,9 @@
 package com.bamin.woorder.member.presentation;
 
 import com.bamin.woorder.AcceptanceTestUtils;
+import com.bamin.woorder.TestRequestMethod;
+import com.bamin.woorder.TestStatusCode;
+import com.bamin.woorder.member.MemberConstants;
 import com.bamin.woorder.member.dto.MemberCreateRequestDto;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
@@ -10,101 +13,93 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
-public class MemberCRUDControllerTest extends AcceptanceTestUtils {
+public class MemberCRUDControllerTest extends AcceptanceTestUtils implements MemberConstants {
 
     @Test
     @DisplayName("[POST] /members, 회원 가입 성공")
     void successfullyCreateUser() {
-        MemberCreateRequestDto requestDto = new MemberCreateRequestDto("woowaluffy");
-
         given().
                 accept(ContentType.JSON).
                 contentType(ContentType.JSON).
-                body(requestDto).
+                body(new MemberCreateRequestDto(SUCCEED_CREATE_NAME)).
         when().
-                post(baseUrl("/members")).
+                post(baseUrl(MEMBERS_SERVLET_BASE_PATH)).
         then().
                 log().ifError().
                 log().ifValidationFails().
                 statusCode(200).
                 contentType(ContentType.JSON).
-                body("path", is("/members")).
-                body("method", is("POST")).
-                body("message", is("회원 가입 성공")).
-                body("data.name", is("woowaluffy")).
-                body("statusCode", is("200")).
+                body("path", is(MEMBERS_SERVLET_BASE_PATH)).
+                body("method", is(TestRequestMethod.POST)).
+                body("message", is(SUCCEED_CREATE_RESPONSE_MESSAGE)).
+                body("data.name", is(SUCCEED_CREATE_NAME)).
+                body("statusCode", is(TestStatusCode.OK)).
                 body("timestamp", is(notNullValue()));
     }
 
     @Test
     @DisplayName("[POST] /members, 회원 가입 실패, 중복 이름")
     void failedCreateUserDuplicatedName() {
-        MemberCreateRequestDto requestDto = new MemberCreateRequestDto("already");
-
         given().
                 accept(ContentType.JSON).
                 contentType(ContentType.JSON).
-                body(requestDto).
+                body(new MemberCreateRequestDto(DUPLICATED_NAME)).
         when().
-                post(baseUrl("/members")).
+                post(baseUrl(MEMBERS_SERVLET_BASE_PATH)).
         then().
                 log().ifError().
                 log().ifValidationFails().
                 statusCode(404).
                 contentType(ContentType.JSON).
-                body("path", is("/members")).
-                body("method", is("POST")).
-                body("message", is("중복된 이름을 사용할 수 없습니다.")).
-                body("data.name", is("already")).
-                body("statusCode", is("404")).
+                body("path", is(MEMBERS_SERVLET_BASE_PATH)).
+                body("method", is(TestRequestMethod.POST)).
+                body("message", is(FAILED_DUPLICATED_RESPONSE_MESSAGE)).
+                body("data.name", is(DUPLICATED_NAME)).
+                body("statusCode", is(TestStatusCode.BAD_REQUEST)).
                 body("timestamp", is(notNullValue()));
     }
 
     @Test
     @DisplayName("[POST] /members, 회원 가입 실패, 이름 글자 수 초과")
     void failedCreateUserNameLength() {
-        MemberCreateRequestDto requestDto = new MemberCreateRequestDto("012345678901234567891");
-
         given().
                 accept(ContentType.JSON).
                 contentType(ContentType.JSON).
-                body(requestDto).
+                body(new MemberCreateRequestDto(UPPER_BOUND_LENGTH_NAME)).
         when().
-                post(baseUrl("/members")).
+                post(baseUrl(MEMBERS_SERVLET_BASE_PATH)).
         then().
                 log().ifError().
                 log().ifValidationFails().
                 statusCode(404).
                 contentType(ContentType.JSON).
-                body("path", is("/members")).
-                body("method", is("POST")).
-                body("message", is("이름은 1자 이상, 20자 이하만 가능합니다.")).
-                body("data.name", is("012345678901234567891")).
-                body("statusCode", is("404")).
+                body("path", is(MEMBERS_SERVLET_BASE_PATH)).
+                body("method", is(TestRequestMethod.POST)).
+                body("message", is(FAILED_LENGTH_RESPONSE_MESSAGE)).
+                body("data.name", is(UPPER_BOUND_LENGTH_NAME)).
+                body("statusCode", is(TestStatusCode.BAD_REQUEST)).
                 body("timestamp", is(notNullValue()));
     }
 
     @Test
     @DisplayName("[POST] /members, 회원 가입 실패, 이름 글자 수 부족")
     void failedCreateUserNoName() {
-        MemberCreateRequestDto requestDto = new MemberCreateRequestDto("");
-
         given().
                 accept(ContentType.JSON).
                 contentType(ContentType.JSON).
-                body(requestDto).
+                body(new MemberCreateRequestDto(LOWER_BOUND_LENGTH_NAME)).
         when().
-                post(baseUrl("/members")).
+                post(baseUrl(MEMBERS_SERVLET_BASE_PATH)).
         then().
                 log().ifError().
                 log().ifValidationFails().
                 statusCode(404).
                 contentType(ContentType.JSON).
-                body("path", is("/members")).
-                body("method", is("POST")).
-                body("message", is("이름은 1자 이상, 20자 이하만 가능합니다.")).
-                body("data.name", is("")).
-                body("statusCode", is("404")).
+                body("path", is(MEMBERS_SERVLET_BASE_PATH)).
+                body("method", is(TestRequestMethod.POST)).
+                body("message", is(FAILED_LENGTH_RESPONSE_MESSAGE)).
+                body("data.name", is(LOWER_BOUND_LENGTH_NAME)).
+                body("statusCode", is(TestStatusCode.BAD_REQUEST)).
                 body("timestamp", is(notNullValue()));
     }
 }

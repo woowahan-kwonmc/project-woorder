3454640b34d5de2071b4990270155c7ec98aebce
woowahan-kwonmc <luffy@woowahan.com>
2020-01-10 15:44:50 +0900

    [#21] feat - 공통 응답 객체 구현

---
+++ src/main/java/com/bamin/woorder/common/dto/DescriptiveResponse.java
@@ -0,0 +1,18 @@
+package com.bamin.woorder.common.dto;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+public interface DescriptiveResponse {
+    String getPath();
+
+    String getMethod();
+
+    String getMessage();
+
+    Map<String, Object> getData();
+
+    String getStatusCode();
+
+    LocalDateTime getTimestamp();
+}
---
+++ src/main/java/com/bamin/woorder/common/dto/DescriptiveResponseDto.java
@@ -0,0 +1,33 @@
+package com.bamin.woorder.common.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class DescriptiveResponseDto implements DescriptiveResponse {
+
+    private String path;
+    private String method;
+    private String message;
+    private Map<String, Object> data;
+    private String statusCode;
+    private LocalDateTime timestamp;
+
+    protected DescriptiveResponseDto(final String path,
+                                     final String method,
+                                     final Map<String, Object> data,
+                                     final String message,
+                                     final String statusCode) {
+        this.path = path;
+        this.method = method;
+        this.data = data;
+        this.message = message;
+        this.statusCode = statusCode;
+        this.timestamp = LocalDateTime.now();
+    }
+}

9ca9e1e11bd64d01fd2028128c9b185e9b37e14b
woowahan-kwonmc <luffy@woowahan.com>
2020-01-10 17:01:52 +0900

    [#21] feat - 멤버 조회 실패 ControllerAdvice 추가

--- src/main/java/com/bamin/woorder/member/presentation/MemberControllerAdvice.java
+++ src/main/java/com/bamin/woorder/member/presentation/MemberControllerAdvice.java
@@ -4,6 +4,7 @@ import com.bamin.woorder.common.dto.ResponseData;
 import com.bamin.woorder.common.dto.ResponseDto;
 import com.bamin.woorder.member.AbstractNameException;
 import com.bamin.woorder.member.application.DuplicatedNameException;
+import com.bamin.woorder.member.application.NotFoundUserException;
 import com.bamin.woorder.member.domain.InvalidNameLengthException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -27,6 +28,18 @@ public class MemberControllerAdvice {
                         .data(ResponseData.builder()
                                 .insert("name", e.getName())
                                 .build())
+                        .statusCode("404")
+                        .build());
+    }
+
+    @ExceptionHandler(NotFoundUserException.class)
+    public ResponseEntity handlingNotFoundUserException(final RuntimeException e, final HttpServletRequest request) {
+        log.error(e.getMessage());
+        return ResponseEntity.status(404)
+                .body(ResponseDto.builder()
+                        .path(request.getServletPath())
+                        .method(request.getMethod())
+                        .message(e.getMessage())
                         .statusCode("404")
                         .build());
     }

f47f160d41735a55451b78b194a9ae8ab835bc2a
woowahan-kwonmc <luffy@woowahan.com>
2020-01-10 15:47:15 +0900

    [#21] feat - 멤버 관련 예외 처리 Controller Advice 구현

---
+++ src/main/java/com/bamin/woorder/member/presentation/MemberControllerAdvice.java
@@ -0,0 +1,33 @@
+package com.bamin.woorder.member.presentation;
+
+import com.bamin.woorder.common.dto.ResponseData;
+import com.bamin.woorder.common.dto.ResponseDto;
+import com.bamin.woorder.member.AbstractNameException;
+import com.bamin.woorder.member.application.DuplicatedNameException;
+import com.bamin.woorder.member.domain.InvalidNameLengthException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestControllerAdvice
+public class MemberControllerAdvice {
+
+    @ExceptionHandler({DuplicatedNameException.class, InvalidNameLengthException.class})
+    public ResponseEntity handlingException(final AbstractNameException e, final HttpServletRequest request) {
+        log.error(e.getMessage());
+        return ResponseEntity.status(404)
+                .body(ResponseDto.builder()
+                        .path(request.getServletPath())
+                        .method(request.getMethod())
+                        .message(e.getMessage())
+                        .data(ResponseData.builder()
+                                .insert("name", e.getName())
+                                .build())
+                        .statusCode("404")
+                        .build());
+    }
+}

772cf6388d007691577e702c1c2dc318533b0f36
woowahan-kwonmc <luffy@woowahan.com>
2020-01-11 16:49:25 +0900

    [#27] feat - 메뉴 목록 조회 기능 구현

--- src/main/java/com/bamin/woorder/menu/application/MenuCRUDService.java
+++ src/main/java/com/bamin/woorder/menu/application/MenuCRUDService.java
@@ -8,6 +8,9 @@ import com.bamin.woorder.menu.dto.MenuResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class MenuCRUDService {
 
@@ -25,9 +28,28 @@ public class MenuCRUDService {
                 .method("POST")
                 .message("메뉴 등록 성공")
                 .data(ResponseData.builder()
-                        .insert("menu", new MenuResponseDto(savedMenu.getNo(), savedMenu.getName(), savedMenu.getPrice()))
+                        .insert("menu", mapMenuResponseDto(savedMenu))
                         .build())
                 .statusCode("200")
                 .build();
+    }
+
+    public ResponseDto selectPageMenus(final int page, final int num) {
+        List<Menu> menus = menuService.selectPageMenus(page, num);
+        List<MenuResponseDto> pageMenus = menus.stream()
+                .map(this::mapMenuResponseDto)
+                .collect(Collectors.toList());
+        return ResponseDto.builder()
+                .path(String.format("/menus?page=%d&num=%d", page, num))
+                .method("GET")
+                .message(String.format("%d 페이지의 메뉴 목록 %d개", page, num))
+                .data(ResponseData.builder()
+                        .insert("menus", pageMenus).build())
+                .statusCode("200")
+                .build();
+    }
+
+    private MenuResponseDto mapMenuResponseDto(final Menu menu) {
+        return new MenuResponseDto(menu.getNo(), menu.getName(), menu.getPrice());
     }
 }
--- src/main/java/com/bamin/woorder/menu/presentation/MenuCRUDController.java
+++ src/main/java/com/bamin/woorder/menu/presentation/MenuCRUDController.java
@@ -1,9 +1,12 @@
 package com.bamin.woorder.menu.presentation;
 
+import com.bamin.woorder.common.dto.PageReadRequestDto;
+import com.bamin.woorder.common.presentation.PageRequestParams;
 import com.bamin.woorder.menu.application.MenuCRUDService;
 import com.bamin.woorder.menu.dto.MenuCreateRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +24,15 @@ public class MenuCRUDController {
     @PostMapping("/menus")
     public ResponseEntity createMenu(@RequestBody final MenuCreateRequestDto requestDto) {
         return ResponseEntity.ok(menuCRUDService.createMenu(requestDto));
+    }
+
+//    @GetMapping("/menus")
+//    public ResponseEntity selectPageMenus(@RequestParam int page, @RequestParam int num) {
+//        return ResponseEntity.ok(menuCRUDService.selectPageMenus(page, num));
+//    }
+
+    @GetMapping("/menus")
+    public ResponseEntity selectPageMenus(@PageRequestParams final PageReadRequestDto requestDto) {
+        return ResponseEntity.ok(menuCRUDService.selectPageMenus(requestDto.getPage(), requestDto.getNum()));
     }
 }

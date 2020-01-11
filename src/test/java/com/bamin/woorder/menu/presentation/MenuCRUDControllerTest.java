57894c56e042d43c6bae238ab07565c09cafc048
woowahan-kwonmc <luffy@woowahan.com>
2020-01-11 16:50:51 +0900

    [#27] test - 메뉴 목록 조회 기능 테스트 추가

--- src/test/java/com/bamin/woorder/menu/presentation/MenuCRUDControllerTest.java
+++ src/test/java/com/bamin/woorder/menu/presentation/MenuCRUDControllerTest.java
@@ -3,6 +3,7 @@ package com.bamin.woorder.menu.presentation;
 import com.bamin.woorder.AcceptanceTestUtils;
 import com.bamin.woorder.TestStatusCode;
 import com.bamin.woorder.common.utils.easyrestassured.EasyExpectBodies;
+import com.bamin.woorder.common.utils.easyrestassured.EasyGivenParams;
 import com.bamin.woorder.common.utils.easyrestassured.EasyRestAssured;
 import com.bamin.woorder.common.utils.easyrestassured.EasyRestAssuredRequestMethod;
 import com.bamin.woorder.menu.dto.MenuCreateRequestDto;
@@ -15,6 +16,7 @@ import static org.hamcrest.CoreMatchers.*;
 class MenuCRUDControllerTest extends AcceptanceTestUtils {
 
     private static final String MENUS_BASE_SERVLET_PATH = "/menus";
+    private static final String PAGE_QUERY_STRING = "?page=1&num=3";
 
     @Test
     @DisplayName("[POST] /menus, 메뉴 등록 성공")
@@ -113,6 +115,46 @@ class MenuCRUDControllerTest extends AcceptanceTestUtils {
                         EasyExpectBodies.builder()
                                 .insert("message", is("메뉴 가격은 0 이하로 설정할 수 없습니다."))
                                 .insert("data.menuPrice", is(notNullValue()))
+                );
+    }
+
+    @Test
+    @DisplayName("[GET] /menus, 해당 페이지의 해당 개수만큼 메뉴 조회 성공")
+    void successfullyGetPageableMenus() {
+        EasyRestAssured
+                .givenParams(
+                        new EasyGivenParams()
+                                .addParam("page", 1)
+                                .addParam("num", 3))
+                .whenRequest(
+                        baseUrl(MENUS_BASE_SERVLET_PATH),
+                        EasyRestAssuredRequestMethod.GET)
+                .thenExpectDescriptiveWith(
+                        TestStatusCode.OK,
+                        MENUS_BASE_SERVLET_PATH + PAGE_QUERY_STRING,
+                        EasyExpectBodies.builder()
+                                .insert("message", is(String.format("%d 페이지의 메뉴 목록 %d개", 1, 3)))
+                                .insert("data.menus.size()", is(3))
+                );
+    }
+
+    @Test
+    @DisplayName("[GET] /menus, 쿼리 스트링에 문자가 들어가면 에러")
+    void failedGetPageableMenus() {
+        EasyRestAssured
+                .givenParams(
+                        new EasyGivenParams()
+                                .addParam("page", 1)
+                                .addParam("num", 3))
+                .whenRequest(
+                        baseUrl(MENUS_BASE_SERVLET_PATH),
+                        EasyRestAssuredRequestMethod.GET)
+                .thenExpectDescriptiveWith(
+                        TestStatusCode.OK,
+                        MENUS_BASE_SERVLET_PATH + PAGE_QUERY_STRING,
+                        EasyExpectBodies.builder()
+                                .insert("message", is(String.format("%d 페이지의 메뉴 목록 %d개", 1, 3)))
+                                .insert("data.menus.size()", is(3))
                 );
     }
 }

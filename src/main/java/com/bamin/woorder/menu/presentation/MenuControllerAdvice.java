23a96cbad985d38aa7686c9b515b55e1243e1088
woowahan-kwonmc <luffy@woowahan.com>
2020-01-11 17:18:47 +0900

    [#27] feat - 존재하는 메뉴 에러 발생시 처리하는 Controller Advice 구현

--- src/main/java/com/bamin/woorder/menu/presentation/MenuControllerAdvice.java
+++ src/main/java/com/bamin/woorder/menu/presentation/MenuControllerAdvice.java
@@ -2,6 +2,7 @@ package com.bamin.woorder.menu.presentation;
 
 import com.bamin.woorder.common.dto.ResponseData;
 import com.bamin.woorder.common.dto.ResponseDto;
+import com.bamin.woorder.menu.application.MenuNotFoundException;
 import com.bamin.woorder.menu.domain.MenuNameLengthException;
 import com.bamin.woorder.menu.domain.MenuPriceRangeException;
 import lombok.extern.slf4j.Slf4j;
@@ -40,6 +41,21 @@ public class MenuControllerAdvice {
                         .message(e.getMessage())
                         .data(ResponseData.builder()
                                 .insert("menuPrice", e.getPrice())
+                                .build())
+                        .statusCode("404")
+                        .build());
+    }
+
+    @ExceptionHandler({MenuNotFoundException.class})
+    public ResponseEntity handlingSelectException(final MenuNotFoundException e, final HttpServletRequest request) {
+        log.error(e.getMessage());
+        return ResponseEntity.status(404)
+                .body(ResponseDto.builder()
+                        .path(request.getServletPath())
+                        .method(request.getMethod())
+                        .message(e.getMessage())
+                        .data(ResponseData.builder()
+                                .insert("requestNo", e.getRequestNo())
                                 .build())
                         .statusCode("404")
                         .build());

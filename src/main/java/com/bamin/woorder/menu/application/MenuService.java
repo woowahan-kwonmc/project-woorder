21a60f8f33f0abc7c6ae26035fa90255fe3febd5
woowahan-kwonmc <luffy@woowahan.com>
2020-01-11 16:48:57 +0900

    [#27] feat - 메뉴를 페이지, 개수에 맞게 조회하는 기능 구현

--- src/main/java/com/bamin/woorder/menu/application/MenuService.java
+++ src/main/java/com/bamin/woorder/menu/application/MenuService.java
@@ -3,7 +3,11 @@ package com.bamin.woorder.menu.application;
 import com.bamin.woorder.menu.domain.Menu;
 import com.bamin.woorder.menu.domain.MenuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MenuService {
@@ -18,5 +22,11 @@ public class MenuService {
     public Menu createMenu(String name, String price) {
         Menu menu = new Menu(name, price);
         return menuRepository.save(menu);
+    }
+
+    public List<Menu> selectPageMenus(final int page, final int num) {
+        return menuRepository.findAll(PageRequest.of(page - 1, num))
+                .stream()
+                .collect(Collectors.toList());
     }
 }

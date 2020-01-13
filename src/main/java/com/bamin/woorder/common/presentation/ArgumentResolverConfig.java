8b278ca4af634e8a9948f75ce72ce58d46ba9291
woowahan-kwonmc <luffy@woowahan.com>
2020-01-13 13:12:03 +0900

    [#39] style - 패키지 변경 및 네이밍 변경

--- src/main/java/com/bamin/woorder/common/presentation/ArgumentResolverConfig.java
+++ src/main/java/com/bamin/woorder/common/presentation/ArgumentResolverConfig.java
@@ -1,6 +1,5 @@
 package com.bamin.woorder.common.presentation;
 
-import com.bamin.woorder.menu.presentation.MenuPageRequestArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +10,6 @@ import java.util.List;
 public class ArgumentResolverConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new MenuPageRequestArgumentResolver());
+        resolvers.add(new SelectPageRequestArgumentResolver());
     }
 }

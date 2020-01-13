6551b600cec25e75438f25ef0c2dbec0cf5dccf0
woowahan-kwonmc <luffy@woowahan.com>
2020-01-13 14:40:36 +0900

    [#39] test - 쿠폰 타입 목록 조회 테스트 추가

--- src/test/java/com/bamin/woorder/coupontype/presentation/CouponTypeCRUDControllerTest.java
+++ src/test/java/com/bamin/woorder/coupontype/presentation/CouponTypeCRUDControllerTest.java
@@ -2,6 +2,7 @@ package com.bamin.woorder.coupontype.presentation;
 
 import com.bamin.woorder.AcceptanceTestUtils;
 import com.bamin.woorder.common.utils.easyrestassured.EasyExpectBodies;
+import com.bamin.woorder.common.utils.easyrestassured.EasyGivenQueryParameters;
 import com.bamin.woorder.common.utils.easyrestassured.EasyRestAssured;
 import com.bamin.woorder.common.utils.easyrestassured.EasyRestAssuredRequestMethod;
 import com.bamin.woorder.coupontype.dto.CouponTypeCreateRequestDto;
@@ -21,8 +22,8 @@ class CouponTypeCRUDControllerTest extends AcceptanceTestUtils {
     @Test
     @DisplayName("[POST] /api/v1/couponTypes, 쿠폰 타입 생성 성공")
     void successfullyCreateCouponType() {
-        String startTime = LocalDateTime.now().plusDays(2L).toString();
-        String endTime = LocalDateTime.now().plusDays(3L).toString();
+        String startTime = LocalDateTime.now().plusDays(2L).withNano(0).toString();
+        String endTime = LocalDateTime.now().plusDays(3L).withNano(0).toString();
         EasyRestAssured
                 .givenBody(new CouponTypeCreateRequestDto(
                         CREATE_COUPON_TYPE_NAME,
@@ -46,8 +47,8 @@ class CouponTypeCRUDControllerTest extends AcceptanceTestUtils {
     @Test
     @DisplayName("[POST] /api/v1/couponTypes, 쿠폰 타입 생성 실패, 기간 설정 오류, 현재보다 이른 시작 시간")
     void failedCreateCouponTypeStartTimeIsEarlierThanCurrentTime() {
-        String startTime = LocalDateTime.now().minusDays(3L).toString();
-        String endTime = LocalDateTime.now().plusDays(3L).toString();
+        String startTime = LocalDateTime.now().minusDays(3L).withNano(0).toString();
+        String endTime = LocalDateTime.now().plusDays(3L).withNano(0).toString();
         EasyRestAssured
                 .givenBody(new CouponTypeCreateRequestDto(
                         CREATE_COUPON_TYPE_NAME,
@@ -68,8 +69,8 @@ class CouponTypeCRUDControllerTest extends AcceptanceTestUtils {
     @Test
     @DisplayName("[POST] /api/v1/couponTypes, 쿠폰 타입 생성 실패, 기간 설정 오류, 현재보다 이른 종료 시간")
     void failedCreateCouponTypeEndTimeIsEarlierThan() {
-        String startTime = LocalDateTime.now().minusDays(3L).toString();
-        String endTime = LocalDateTime.now().minusDays(3L).toString();
+        String startTime = LocalDateTime.now().minusDays(3L).withNano(0).toString();
+        String endTime = LocalDateTime.now().minusDays(3L).withNano(0).toString();
         EasyRestAssured
                 .givenBody(new CouponTypeCreateRequestDto(
                         CREATE_COUPON_TYPE_NAME,
@@ -90,7 +91,7 @@ class CouponTypeCRUDControllerTest extends AcceptanceTestUtils {
     @Test
     @DisplayName("[POST] /api/v1/couponTypes, 쿠폰 타입 생성 실패, 기간 설정 오류, 시작 시간과 종료 시간 동일")
     void failedCreateCouponTypeSameStartAndEnd() {
-        String sameTime = LocalDateTime.now().plusDays(3L).toString();
+        String sameTime = LocalDateTime.now().plusDays(3L).withNano(0).toString();
         EasyRestAssured
                 .givenBody(new CouponTypeCreateRequestDto(
                         CREATE_COUPON_TYPE_NAME,
@@ -111,8 +112,8 @@ class CouponTypeCRUDControllerTest extends AcceptanceTestUtils {
     @Test
     @DisplayName("[POST] /api/v1/couponTypes, 쿠폰 타입 생성 실패, 기간 설정 오류, 기간이 현재 보다 빠름")
     void failedCreateCouponTypePeriodIsEarlierThanNow() {
-        String startTime = LocalDateTime.now().minusDays(3L).toString();
-        String endTime = LocalDateTime.now().minusDays(1L).toString();
+        String startTime = LocalDateTime.now().minusDays(3L).withNano(0).toString();
+        String endTime = LocalDateTime.now().minusDays(1L).withNano(0).toString();
         EasyRestAssured
                 .givenBody(new CouponTypeCreateRequestDto(
                         CREATE_COUPON_TYPE_NAME,
@@ -128,5 +129,19 @@ class CouponTypeCRUDControllerTest extends AcceptanceTestUtils {
                                 .insert("data.startTime", is(startTime))
                                 .insert("data.endTime", is(endTime))
                                 .build());
+    }
+
+    @Test
+    @DisplayName("[GET] /api/v1/couponTypes/all, 쿠폰 타입 리스트 조회 성공")
+    void successfullySelectPageCouponTypes() {
+        EasyRestAssured
+                .givenParams(new EasyGivenQueryParameters()
+                        .addParam("page", 1)
+                        .addParam("num", 3))
+                .whenRequest(baseUrl(COUPON_TYPE_ALL_SERVLET_PATH), EasyRestAssuredRequestMethod.GET)
+                .thenExpectDescriptiveWith(OK, COUPON_TYPE_ALL_SERVLET_PATH,
+                        EasyExpectBodies.builder()
+                                .insert("message", is(String.format(COUPON_TYPES_SELECT_SUCCEED_RESPONSE_MESSAGE, 1, 3)))
+                                .insert("data.couponTypes.size()", is(3)));
     }
 }
--- src/test/java/com/bamin/woorder/coupontype/presentation/CouponTypeConstants.java
+++ src/test/java/com/bamin/woorder/coupontype/presentation/CouponTypeConstants.java
@@ -5,9 +5,14 @@ class CouponTypeConstants {
     static final String CREATE_COUPON_TYPE_NAME = "민철이가 쏜다";
     static final String CREATE_COUPON_TYPE_COUNT = "100";
     static final String CREATE_COUPON_TYPE_DISCOUNT = "10000";
+
     static final int CREATE_COUPON_TYPE_COUNT_INT = 100;
     static final int CREATE_COUPON_TYPE_DISCOUNT_INT = 10000;
+
     static final String COUPON_TYPE_CREATE_SUCCEED_RESPONSE_MESSAGE = "쿠폰 종류 생성 성공";
     static final String COUPON_TYPE_PERIOD_FAILED_RESPONSE_MESSAGE = "쿠폰 사용 가능 기간 설정을 확인해주세요.";
+    static final String COUPON_TYPES_SELECT_SUCCEED_RESPONSE_MESSAGE = "쿠폰 타입 %d 페이지 %d 개 조회 성공";
+
     static final String COUPON_TYPE_SERVLET_BASE_PATH = "/api/v1/couponTypes";
+    static final String COUPON_TYPE_ALL_SERVLET_PATH = "/api/v1/couponTypes/all";
 }

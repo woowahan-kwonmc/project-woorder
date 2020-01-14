65570510d2fc8f23edb556d40c6f8b92672c9ad7
luffy <luffy@woowahan.com>
2020-01-14 12:27:14 +0900

    [#35] test - 쿠폰 타입 생성 가능, 쿠폰 여부 체크 테스트 추가

--- src/test/java/com/bamin/woorder/coupontype/domain/CouponTypeTest.java
+++ src/test/java/com/bamin/woorder/coupontype/domain/CouponTypeTest.java
@@ -1,76 +1,141 @@
 package com.bamin.woorder.coupontype.domain;
 
-import com.bamin.woorder.coupontype.domain.exception.CouponTypeCountRangeException;
-import com.bamin.woorder.coupontype.domain.exception.CouponTypeDiscountAmountException;
-import com.bamin.woorder.coupontype.domain.exception.CouponTypeNameLengthException;
-import com.bamin.woorder.coupontype.domain.exception.CouponTypeUsablePeriodException;
+import com.bamin.woorder.coupontype.domain.exception.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CouponTypeTest {
 
+    public static final String COUPON_TYPE_NAME = "민철이가 쏜다.";
+    public static final String COUPON_TYPE_COUNT = "10";
+    public static final String COUPON_TYPE_DISCOUNT = "1000";
+    public static final boolean COUPON_TYPE_HAS_CODE_CODE_TYPE = true;
+
     @Test
     @DisplayName("CouponType 생성 성공")
     void successfullyConstruct() {
-        assertDoesNotThrow(() -> new CouponType(
-                "민철이가 쏜다.",
-                "1000",
-                "10",
-                true,
-                LocalDateTime.now().plusDays(2L).toString(),
-                LocalDateTime.now().plusDays(3L).toString()));
+        assertDoesNotThrow(() -> CouponType.builder()
+                .couponTypeName(COUPON_TYPE_NAME)
+                .couponTypeCount(COUPON_TYPE_COUNT)
+                .couponTypeDiscount(COUPON_TYPE_DISCOUNT)
+                .couponTypeHasCode(COUPON_TYPE_HAS_CODE_CODE_TYPE)
+                .startTime(LocalDateTime.now().plusDays(2L).toString())
+                .endTime(LocalDateTime.now().plusDays(3L).toString())
+                .build());
     }
 
     @Test
     @DisplayName("CouponType 생성 실패, 이름")
     void failedConstructWithName() {
-        assertThrows(CouponTypeNameLengthException.class, () -> new CouponType(
-                "",
-                "1000",
-                "10",
-                true,
-                LocalDateTime.now().plusDays(2L).toString(),
-                LocalDateTime.now().plusDays(3L).toString()));
+        assertThrows(CouponTypeNameLengthException.class, () -> CouponType.builder()
+                .couponTypeName("")
+                .couponTypeCount(COUPON_TYPE_COUNT)
+                .couponTypeDiscount(COUPON_TYPE_DISCOUNT)
+                .couponTypeHasCode(COUPON_TYPE_HAS_CODE_CODE_TYPE)
+                .startTime(LocalDateTime.now().plusDays(2L).toString())
+                .endTime(LocalDateTime.now().plusDays(3L).toString())
+                .build());
     }
 
     @Test
     @DisplayName("CouponType 생성 실패, 수량")
     void failedConstructWithCount() {
-        assertThrows(CouponTypeCountRangeException.class, () -> new CouponType(
-                "민철이가 쏜다.",
-                "0",
-                "10",
-                true,
-                LocalDateTime.now().plusDays(2L).toString(),
-                LocalDateTime.now().plusDays(3L).toString()));
+        assertThrows(CouponTypeCountRangeException.class, () -> CouponType.builder()
+                .couponTypeName(COUPON_TYPE_NAME)
+                .couponTypeCount("0")
+                .couponTypeDiscount(COUPON_TYPE_DISCOUNT)
+                .couponTypeHasCode(COUPON_TYPE_HAS_CODE_CODE_TYPE)
+                .startTime(LocalDateTime.now().plusDays(2L).toString())
+                .endTime(LocalDateTime.now().plusDays(3L).toString())
+                .build());
     }
 
     @Test
     @DisplayName("CouponType 생성 실패, 가격")
     void failedConstructWithDiscount() {
-        assertThrows(CouponTypeDiscountAmountException.class, () -> new CouponType(
-                "민철이가 쏜다.",
-                "1",
-                "0",
-                true,
-                LocalDateTime.now().plusDays(2L).toString(),
-                LocalDateTime.now().plusDays(3L).toString()));
+        assertThrows(CouponTypeDiscountAmountException.class, () -> CouponType.builder()
+                .couponTypeName(COUPON_TYPE_NAME)
+                .couponTypeCount(COUPON_TYPE_COUNT)
+                .couponTypeDiscount("0")
+                .couponTypeHasCode(COUPON_TYPE_HAS_CODE_CODE_TYPE)
+                .startTime(LocalDateTime.now().plusDays(2L).toString())
+                .endTime(LocalDateTime.now().plusDays(3L).toString())
+                .build());
     }
 
     @Test
     @DisplayName("CouponType 생성 실패, 기간")
     void failedConstructWithPeriod() {
-        assertThrows(CouponTypeUsablePeriodException.class, () -> new CouponType(
-                "민철이가 쏜다.",
-                "1",
-                "1",
-                true,
-                LocalDateTime.now().minusDays(2L).toString(),
-                LocalDateTime.now().plusDays(3L).toString()));
+        assertThrows(CouponTypeUsablePeriodException.class, () -> CouponType.builder()
+                .couponTypeName(COUPON_TYPE_NAME)
+                .couponTypeCount(COUPON_TYPE_COUNT)
+                .couponTypeDiscount(COUPON_TYPE_DISCOUNT)
+                .couponTypeHasCode(COUPON_TYPE_HAS_CODE_CODE_TYPE)
+                .startTime(LocalDateTime.now().minusDays(2L).toString())
+                .endTime(LocalDateTime.now().plusDays(3L).toString())
+                .build());
+    }
+
+    @Test
+    @DisplayName("CouponType, Coupon 생성 가능 기간 확인")
+    void successfullyCheckCreatablePeriod() {
+        CouponType couponType = CouponType.builder()
+                .couponTypeName(COUPON_TYPE_NAME)
+                .couponTypeCount(COUPON_TYPE_COUNT)
+                .couponTypeDiscount(COUPON_TYPE_DISCOUNT)
+                .couponTypeHasCode(COUPON_TYPE_HAS_CODE_CODE_TYPE)
+                .startTime(LocalDateTime.now().plusDays(2L).toString())
+                .endTime(LocalDateTime.now().plusDays(3L).toString())
+                .build();
+        assertThat(couponType.checkCreatablePeriod()).isTrue();
+    }
+
+    @Test
+    @DisplayName("CouponType, Coupon 생성 가능 기간 확인 성공")
+    void successfullyCheckCouponTypeCountExcess() {
+        CouponType couponType = CouponType.builder()
+                .couponTypeName(COUPON_TYPE_NAME)
+                .couponTypeCount(COUPON_TYPE_COUNT)
+                .couponTypeDiscount(COUPON_TYPE_DISCOUNT)
+                .couponTypeHasCode(COUPON_TYPE_HAS_CODE_CODE_TYPE)
+                .startTime(LocalDateTime.now().plusDays(2L).toString())
+                .endTime(LocalDateTime.now().plusDays(3L).toString())
+                .build();
+        assertThat(couponType.getTotalCountAfterGenerate(1L, 1L)).isEqualTo(2L);
+    }
+
+    @Test
+    @DisplayName("CouponType, Coupon 생성 가능 기간 확인 실패, 초과")
+    void failedCheckCouponTypeCountExcess() {
+        CouponType couponType = CouponType.builder()
+                .couponTypeName(COUPON_TYPE_NAME)
+                .couponTypeCount(COUPON_TYPE_COUNT)
+                .couponTypeDiscount(COUPON_TYPE_DISCOUNT)
+                .couponTypeHasCode(COUPON_TYPE_HAS_CODE_CODE_TYPE)
+                .startTime(LocalDateTime.now().plusDays(2L).toString())
+                .endTime(LocalDateTime.now().plusDays(3L).toString())
+                .build();
+        assertThrows(CouponTypeCountExcessException.class,
+                () -> couponType.getTotalCountAfterGenerate(10L, 1L));
+    }
+
+    @Test
+    @DisplayName("CouponType, 다운로드형 확인 실패, 코드형 쿠폰 타입")
+    void successfullyCheckDownloadable() {
+        CouponType couponType = CouponType.builder()
+                .couponTypeName(COUPON_TYPE_NAME)
+                .couponTypeCount(COUPON_TYPE_COUNT)
+                .couponTypeDiscount(COUPON_TYPE_DISCOUNT)
+                .couponTypeHasCode(COUPON_TYPE_HAS_CODE_CODE_TYPE)
+                .startTime(LocalDateTime.now().plusDays(2L).toString())
+                .endTime(LocalDateTime.now().plusDays(3L).toString())
+                .build();
+        assertThrows(IllegalCouponTypeException.class, couponType::checkDownloadable);
     }
 }

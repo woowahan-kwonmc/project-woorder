ec46f9afefe9f4b9832e7c2428adb0923c20acbe
luffy <luffy@woowahan.com>
2020-01-14 21:52:48 +0900

    [#35] feat - CouponPageRequestArgumentResolver 구현

--- src/main/java/com/bamin/woorder/common/presentation/ArgumentResolverConfig.java
+++ src/main/java/com/bamin/woorder/common/presentation/ArgumentResolverConfig.java
@@ -1,5 +1,6 @@
 package com.bamin.woorder.common.presentation;
 
+import com.bamin.woorder.coupon.presentation.CouponPageRequestArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,5 +12,6 @@ public class ArgumentResolverConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new SelectPageRequestArgumentResolver());
+        resolvers.add(new CouponPageRequestArgumentResolver());
     }
 }

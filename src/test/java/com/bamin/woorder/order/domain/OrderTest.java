a85e7391fee5a71d24a0310c48b488a4c0702339
woowahan-kwonmc <luffy@woowahan.com>
2020-01-19 23:42:22 +0900

    [#49] test - 주문 도메인 단위 테스트

---
+++ src/test/java/com/bamin/woorder/order/domain/OrderTest.java
@@ -0,0 +1,32 @@
+package com.bamin.woorder.order.domain;
+
+import com.bamin.woorder.member.domain.Member;
+import com.bamin.woorder.menu.domain.Menu;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class OrderTest {
+
+    @Test
+    @DisplayName("Order 생성 성공")
+    void succeedConstructOrder() {
+        assertDoesNotThrow(() -> Order.builder()
+                .quantity(1)
+                .savedMember(new Member("권민철"))
+                .savedMenu(new Menu("양념치킨", "18000"))
+                .build());
+    }
+
+    @Test
+    @DisplayName("Order 생성 실패, 수량 범위 오류")
+    void failedConstructQuantityRange() {
+        assertThrows(OrderQuantityRangeException.class, () -> Order.builder()
+                .quantity(0)
+                .savedMember(new Member("권민철"))
+                .savedMenu(new Menu("양념치킨", "18000"))
+                .build());
+    }
+}

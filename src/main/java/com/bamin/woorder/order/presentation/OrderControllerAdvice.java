52248b2c62226ffc533163cea27dac0405019925
woowahan-kwonmc <luffy@woowahan.com>
2020-01-20 14:51:48 +0900

    [#50] feat - 주문 조회 기능 추가

--- src/main/java/com/bamin/woorder/order/presentation/OrderControllerAdvice.java
+++ src/main/java/com/bamin/woorder/order/presentation/OrderControllerAdvice.java
@@ -1,8 +1,10 @@
 package com.bamin.woorder.order.presentation;
 
+import com.bamin.woorder.common.dto.ResponseData;
 import com.bamin.woorder.common.dto.ResponseDto;
 import com.bamin.woorder.common.dto.ResponseDtoStatusCode;
 import com.bamin.woorder.order.OrderException;
+import com.bamin.woorder.order.application.NotFoundOrderException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,7 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 public class OrderControllerAdvice {
 
     @ExceptionHandler({OrderException.class})
-    public ResponseEntity handlingNameException(final OrderException e, final HttpServletRequest request) {
+    public ResponseEntity handlingOrderException(final OrderException e, final HttpServletRequest request) {
         log.error(e.getMessage());
         return ResponseEntity.status(ResponseDtoStatusCode.BAD_REQUEST)
                 .body(ResponseDto.builder()
@@ -23,6 +25,21 @@ public class OrderControllerAdvice {
                         .method(request.getMethod())
                         .message(e.getMessage())
                         .statusCode(ResponseDtoStatusCode.BAD_REQUEST)
+                        .build());
+    }
+
+    @ExceptionHandler({NotFoundOrderException.class})
+    public ResponseEntity handlingNotFound(final NotFoundOrderException e, final HttpServletRequest request) {
+        log.error(e.getMessage());
+        return ResponseEntity.status(ResponseDtoStatusCode.NOT_FOUND)
+                .body(ResponseDto.builder()
+                        .path(request.getServletPath())
+                        .method(request.getMethod())
+                        .message(e.getMessage())
+                        .data(ResponseData.builder()
+                                .insert("requestNo", e.getRequestNo())
+                                .build())
+                        .statusCode(ResponseDtoStatusCode.NOT_FOUND)
                         .build());
     }
 }

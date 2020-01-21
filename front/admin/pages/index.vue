4204187d14f90e372bb9b9a47e177c7a83a428a7
woowahan-kwonmc <luffy@woowahan.com>
2020-01-22 01:17:52 +0900

    [#65] feat - 메인 화면 수정

--- front/admin/pages/index.vue
+++ front/admin/pages/index.vue
@@ -1,65 +1,26 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        woorder-admin
-      </h1>
-      <h2 class="subtitle">
-        woowahan pilot project woorder
-      </h2>
-      <div class="links">
-        <a
-          class="button--green"
-          href="https://nuxtjs.org/"
-          target="_blank">Documentation</a>
-        <a
-          class="button--grey"
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank">GitHub</a>
-      </div>
+  <default-template>
+    <div class="index-buttons">
+      <v-btn class="ma-2" color="indigo" dark tile to="/coupontype">쿠폰 타입</v-btn>
+      <v-btn class="ma-2" color="indigo" dark tile to="/coupon">쿠폰</v-btn>
+      <v-btn class="ma-2" color="indigo" dark tile to="/order">주문</v-btn>
+      <v-btn class="ma-2" color="indigo" dark tile to="/payment">결제</v-btn>
     </div>
-  </section>
+  </default-template>
 </template>
 
 <script>
-    import AppLogo from '~/components/AppLogo.vue'
+    import DefaultTemplate from '../components/DefaultTemplate'
 
     export default {
         components: {
-            AppLogo
+            DefaultTemplate,
         }
     }
 </script>
 
-<style>
-  .container {
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-  }
-
-  .title {
-    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-    display: block;
-    font-weight: 300;
-    font-size: 100px;
-    color: #35495e;
-    letter-spacing: 1px;
-  }
-
-  .subtitle {
-    font-weight: 300;
-    font-size: 42px;
-    color: #526488;
-    word-spacing: 5px;
-    padding-bottom: 15px;
-  }
-
-  .links {
-    padding-top: 15px;
+<style scoped>
+  .index-buttons {
+    margin: 16px;
   }
 </style>
-

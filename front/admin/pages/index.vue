107a539cefbfa98fd39dce72cec1791e5a118ec5
woowahan-kwonmc <luffy@woowahan.com>
2020-01-21 14:45:33 +0900

    [#65] init - 어드민 Nuxt 프로젝트 생성

---
+++ front/admin/pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="container">
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        woorder-admin
+      </h1>
+      <h2 class="subtitle">
+        woowahan pilot project woorder
+      </h2>
+      <div class="links">
+        <a
+          class="button--green"
+          href="https://nuxtjs.org/"
+          target="_blank">Documentation</a>
+        <a
+          class="button--grey"
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+    import AppLogo from '~/components/AppLogo.vue'
+
+    export default {
+        components: {
+            AppLogo
+        }
+    }
+</script>
+
+<style>
+  .container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+  .title {
+    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+
+  .subtitle {
+    font-weight: 300;
+    font-size: 42px;
+    color: #526488;
+    word-spacing: 5px;
+    padding-bottom: 15px;
+  }
+
+  .links {
+    padding-top: 15px;
+  }
+</style>
+

63db1a63e8496f26cbe4dedb043d91458d3d7176
woowahan-kwonmc <luffy@woowahan.com>
2020-01-23 02:30:44 +0900

    [#46] feat - 쿠폰 화면 기능 추가

--- front/user/src/views/Coupon.vue
+++ front/user/src/views/Coupon.vue
@@ -22,10 +22,27 @@
 
         <v-col>
             <v-toolbar>
-                <v-toolbar-title color="black" flat>코드 쿠폰 등록</v-toolbar-title>
+                <v-toolbar-title color="black" flat>다운로드 쿠폰 받기</v-toolbar-title>
             </v-toolbar>
             <div class="default-box-shadow" style="padding: 16px;">
-                <div>쿠폰 받기</div>
+                <div :key="i"
+                     style="padding: 4px; border-bottom: 1px solid #a9a9a9; display: flex; flex-direction: row;"
+                     v-for="(coupon, i) in this.download.coupons">
+                    <div style="flex: 6; display: flex; flex-direction: column;">
+                        <div>쿠폰 이름 - {{ coupon.name }}</div>
+                        <div>할인 금액 - {{ coupon.discount }}</div>
+                    </div>
+                    <div style="flex: 1; align-self: center">
+                        <v-btn @click="getDownloadCoupon(coupon.no)" color="black" dark fab small>받기</v-btn>
+                    </div>
+                </div>
+                <div style="padding: 16px; text-align: center; color: red;" v-if="this.download.errorMessage">
+                    {{this.download.errorMessage}}
+                </div>
+                <div style="padding: 16px; text-align: left; color: black;" v-if="this.download.enrolledCoupon">
+                    <div>쿠폰 이름 - {{this.download.enrolledCoupon.name}}</div>
+                    <div>할인 금액 - {{this.download.enrolledCoupon.discount}} 원</div>
+                </div>
             </div>
         </v-col>
     </div>
@@ -34,6 +51,7 @@
 <script>
     import NeedLogin from "../components/NeedLogin";
     import {couponController} from "../common/controller/CouponController";
+    import {couponTypeController} from "../common/controller/CouponTypeController";
 
     export default {
         data: () => ({
@@ -42,10 +60,27 @@
                 errorMessage: '',
                 enrolledCoupon: null,
             },
+            download: {
+                page: 1,
+                num: 10,
+                coupons: [],
+                enrolledCoupon: null,
+                errorMessage: '',
+            }
         }),
 
         components: {
             NeedLogin,
+        },
+
+        async beforeMount() {
+            try {
+                const response = await couponTypeController.selectPageCouponTypes(this.download.page, this.download.num)
+                this.download.coupons = response.data.couponTypes
+            } catch (e) {
+                // eslint-disable-next-line no-console
+                console.log(e)
+            }
         },
 
         methods: {
@@ -58,18 +93,31 @@
                     this.code.errorMessage = '쿠폰 코드 길이를 확인해주세요.'
                     return
                 }
-                // eslint-disable-next-line no-console
-                console.log(this.code.coupon)
                 try {
                     const memberNo = this.$store.state.member.memberNo
                     const response = await couponController.enrollCodeCoupon(memberNo, this.code.coupon)
                     this.code.errorMessage = null
-                    // eslint-disable-next-line no-console
-                    console.log(response.data.coupon)
                     this.code.enrolledCoupon = response.data.coupon
                 } catch (e) {
                     if (e.response.data) {
                         this.code.errorMessage = e.response.data.message
+                        return
+                    }
+                    // eslint-disable-next-line no-console
+                    console.log(e)
+                }
+            },
+
+            async getDownloadCoupon(couponTypeNo) {
+                this.download.enrolledCoupon = null
+                try {
+                    const memberNo = this.$store.state.member.memberNo
+                    const response = await couponController.downloadCoupon(memberNo, couponTypeNo)
+                    this.download.errorMessage = null
+                    this.download.enrolledCoupon = response.data.coupon
+                } catch (e) {
+                    if (e.response.data) {
+                        this.download.errorMessage = e.response.data.message
                         return
                     }
                     // eslint-disable-next-line no-console

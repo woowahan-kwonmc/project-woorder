22b32df6caca08c86fc4ca635a039b0a5a4524b6
woowahan-kwonmc <luffy@woowahan.com>
2020-01-22 10:59:23 +0900

    [#65] feat - 쿠폰 타입 관련 컴포넌트 구현

---
+++ front/admin/components/coupontype/CouponTypeExpansionPanelElement.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="display: flex; flex-direction: row;">
+    <div style="flex: 1;">
+      {{couponType.no}}
+    </div>
+    <div style="flex: 4;">
+      {{couponType.name}}
+    </div>
+    <div style="flex: 4">
+      할인 금액 {{couponType.discount}} 원
+    </div>
+    <div style="flex: 2">
+      {{couponType.hasCode ? '코드 쿠폰' : '다운로드 쿠폰'}}
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            couponType: {
+                type: Object,
+                required: true,
+            }
+        }
+    }
+</script>

e30117b02f3b60a61a3c1cd30625dcd726e9aa8a
woowahan-kwonmc <luffy@woowahan.com>
2020-01-22 15:54:23 +0900

    [#65] feat - 주문 API 조회 기능 구현

---
+++ front/admin/middleware/common/OrderStatus.js
@@ -0,0 +1,11 @@
+const ALL = "ALL"
+const CREATE = "CREATE"
+const IN_PROGRESS = "IN_PROGRESS"
+const CANCELED = "CANCELED"
+
+export const OrderStatus = {
+  ALL,
+  CREATE,
+  IN_PROGRESS,
+  CANCELED
+}

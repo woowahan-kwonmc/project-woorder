26be013ef1bfd37656222e5a507e289cb4346a83
woowahan-kwonmc <luffy@woowahan.com>
2020-01-22 16:46:42 +0900

    [#46] feat - 멤버 정보 Vuex 설정

--- front/user/src/store/index.js
+++ front/user/src/store/index.js
@@ -4,8 +4,30 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {},
-    mutations: {},
-    actions: {},
-    modules: {}
+    state: {
+        member: null,
+        isLoggedIn: false,
+    },
+
+    mutations: {
+        setMember(state, member) {
+            state.member = member
+            state.isLoggedIn = !!member
+        },
+
+        removeMember(state) {
+            state.member = null
+            state.isLoggedIn = false
+        }
+    },
+
+    actions: {
+        setMember({commit}, member) {
+            commit('setMember', member)
+        },
+
+        removeMember({commit}) {
+            commit('removeMember')
+        }
+    }
 })

8634b964cc342dd1c2e751e1e338bf47a704f8c9
woowahan-kwonmc <luffy@woowahan.com>
2020-01-23 10:46:08 +0900

    [#74] fix - import 문 수정

--- src/main/java/com/bamin/woorder/payment/presentation/PaymentCRUDController.java
+++ src/main/java/com/bamin/woorder/payment/presentation/PaymentCRUDController.java
@@ -4,10 +4,7 @@ import com.bamin.woorder.payment.application.PaymentCRUDService;
 import com.bamin.woorder.payment.dto.PaymentCreateRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1")
 @RestController
